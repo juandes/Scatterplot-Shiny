9dd35f2377abfeb870afe2050094d3bbdc7b2e9d
Juan De Dios Santos <juan.d.diossantos@gmail.com>
2015-08-07 00:06:06 +0200

    Removed empty space

--- server.R
+++ server.R
@@ -1,11 +1,7 @@
-
 shinyServer(function(input, output, session) {
   # Get the data from the variables declared on the ui.R file
   df <- reactive({mtcars[, c(input$xCol, input$yCol)]})
-  
-  # Build the clusters
-  #clusters <- reactive({kmeans(df(), input$clusters)})
-  
+
   # Create the plot
   output$plot <- renderPlot({plot(df(), pch = 20, cex = 3, col = "blue",
                                   main = "1974 Motor Trend dataset")})
